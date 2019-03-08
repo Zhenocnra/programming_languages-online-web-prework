0a857075dd96c4b5b3a704b3873598d3295db202
Zoncharne Lee <zoncharne@gmail.com>
2019-03-08 03:25:01 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,5 +1,15 @@
 def reformat_languages(languages)
   new_hash = {}
-  
-  new_hash
+  languages.each do |category, lang|
+    lang.each do |name, style|
+      style.each do |key, value|
+        new_hash[name] = {
+          :type => Key,
+          :style => []
+        }
+        new_hash[name][:style] << category
+      end
+    end
+  end
+  new_hash[]
 end
